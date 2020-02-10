dc74b27ea02fd84b529fd9f20feb44f350e40977
Harrison Levin <harrison483@gmail.com>
2020-02-10 10:52:35 -0700

    remove api key

--- app/models/api.rb
+++ app/models/api.rb
@@ -7,7 +7,7 @@ class API
   def initialize(house)
     @conn = Faraday.new(
       url: 'https://www.potterapi.com/v1/',
-      params: {key: '$2a$10$GRjvbsLBFsY3O04xp977yOhFKS0a0TQ0C0WeTMDGDyAsU3fJ6rFy6'})
+      params: {key: ENV['POTTER_API']})
     @house = house
   end
 
